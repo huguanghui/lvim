b122636d37e63f9f955aa8c2825bc58aed48dfb1
huguanghui <522146829@qq.com>
2021-08-01 14:46:42 +0800

    feat(c/c++) c/c++自动格式化

--- config.lua
+++ config.lua
@@ -12,13 +12,31 @@ lvim.leader = "space"
 vim.cmd [[
   nnoremap <esc><esc> <cmd>nohlsearch<cr>
   nnoremap Y y$
+  nnoremap <C-]> <cmd>lua vim.lsp.buf.definition()<cr> 
   vnoremap p "_dP
 ]]
 
 -- LSP
 lvim.lsp.diagnostics.virtual_text = false
 lvim.lsp.override = {"java"}
--- require("user.json_schemas").setup()
+
+lvim.lang.c.formatters = {
+      {
+        -- @usage can be clang_format or uncrustify
+        exe = "clang_format",
+        args = {},
+        stdin = true,
+      },
+}
+
+lvim.lang.cpp.formatters = {
+      {
+        -- @usage can be clang_format or uncrustify
+        exe = "clang_format",
+        args = {},
+        stdin = true,
+      },
+}
 
 -- Builtins
 lvim.builtin.dashboard.active = true
@@ -30,14 +48,15 @@ lvim.builtin.terminal.active = true
 lvim.builtin.which_key.mappings.l.d = { "<cmd>TroubleToggle<cr>", "Diagnostics" }
 lvim.builtin.which_key.mappings.l.R = { "<cmd>TroubleToggle lsp_references<cr>", "References" }
 lvim.builtin.which_key.mappings.l.o = { "<cmd>SymbolsOutline<cr>", "Outline" }
-lvim.builtin.which_key.mappings["z"] = { "<cmd>ZenMode<cr>", "Zen" }
+-- lvim.builtin.which_key.mappings["z"] = { "<cmd>ZenMode<cr>", "Zen" }
 lvim.builtin.which_key.mappings["r"] = {
   name = "Replace",
   r = { "<cmd>lua require('spectre').open()<cr>", "Replace" },
   w = { "<cmd>lua require('spectre').open_visual({select_word=true})<cr>", "Replace Word" },
   f = { "<cmd>lua require('spectre').open_file_search()<cr>", "Replace Buffer" },
 }
-lvim.builtin.which_key.mappings.f = { "<cmd>lua require('lir.float').toggle()<cr>", "Files" }
+lvim.builtin.which_key.mappings.G = {"<cmd>Dox<cr>", "Doxygen"}
+-- lvim.builtin.which_key.mappings.f = { "<cmd>lua require('lir.float').toggle()<cr>", "Files" }
 
 -- Treesitter
 lvim.builtin.treesitter.ensure_installed = "maintained"

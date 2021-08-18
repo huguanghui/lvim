6eacb7a19f5065bd6f1e42afa95cf0594e165a4c
huguanghui <522146829@qq.com>
2021-08-19 07:11:35 +0800

    fix(outline) 解决outline不生效问题

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -3,23 +3,27 @@ local M = {}
 M.config = function ()
   lvim.plugins = {
     {"lunarvim/colorschemes"},
-  {"editorconfig/editorconfig-vim"},
-  {"psliwka/vim-smoothie"},
-  {
-    -- 快速移动
-    'justinmk/vim-sneak',
-    -- event = "BufWinEnter",
-    setup = function ()
-      require("user.sneak").setup()
-    end,
-  },
-  {
-    "babaybus/DoxygenToolkit.vim",
-    event = "VimEnter",
-    config = function ()
-      require("user.doxygen").setup()
-    end,
-  },
+    {"editorconfig/editorconfig-vim"},
+    {"psliwka/vim-smoothie"},
+    {
+      -- 快速移动
+      'justinmk/vim-sneak',
+      -- event = "BufWinEnter",
+      setup = function ()
+        require("user.sneak").setup()
+      end,
+    },
+    {
+      "babaybus/DoxygenToolkit.vim",
+      event = "VimEnter",
+      config = function ()
+        require("user.doxygen").setup()
+      end,
+    },
+    {
+      "simrat39/symbols-outline.nvim",
+      cmd = "SymbolsOutline",
+    },
   }
 end
 

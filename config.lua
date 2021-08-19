fb485bc26c832eea51cb1e4c519fbb8e0cb19861
huguanghui <522146829@qq.com>
2021-08-20 07:25:54 +0800

    feat 结构优化

--- config.lua
+++ config.lua
@@ -1,32 +1,36 @@
 -- general
-lvim.format_on_save = true
+lvim.format_on_save = false
+lvim.leader = " "
 lvim.colorscheme = "darkplus"
 lvim.debug = false
 -- lvim.log.level = "debug"
 -- lvim.lint_on_save = true
 lvim.transparent_window = false
-
--- Default options
 vim.opt.relativenumber = true
-vim.opt.wrap = false
+vim.opt.wrap = true
 vim.opt.timeoutlen = 200
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+vim.opt.foldlevel = 5
 vim.opt.guifont = "FiraCode Nerd Font:h13"
 vim.opt.cmdheight = 1
+vim.opt.list = true
 
--- keymappings
-lvim.leader = "space"
-
--- LSP
-lvim.lsp.diagnostics.virtual_text = false
-lvim.lsp.override = {"java"}
-
--- Builtins
+-- Customization
+-- =============================================
 lvim.builtin.dashboard.active = true
 lvim.builtin.terminal.active = true
 -- lvim.builtin.nvimtree.side = "left"
 -- lvim.builtin.nvimtree.show_icons.git = 0
-
 require("user.builtin").config()
+
+-- StatusLine
+-- =============================================
+
+-- Language Specific
+-- =========================================
+lvim.lsp.diagnostics.virtual_text = false
+lvim.lsp.override = {"java"}
 
 -- Additional Plugins
 -- =============================================

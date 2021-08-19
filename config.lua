7348aba410a255a885a2f48a4970c1249e0b38b9
huguanghui <522146829@qq.com>
2021-08-20 07:17:03 +0800

    feat(keybings) 调整keybinding位置

--- config.lua
+++ config.lua
@@ -16,16 +16,6 @@ vim.opt.cmdheight = 1
 -- keymappings
 lvim.leader = "space"
 
-vim.cmd [[
-  nnoremap <esc><esc> <cmd>nohlsearch<cr>
-  nnoremap Y y$
-  nnoremap <C-]> <cmd>lua vim.lsp.buf.definition()<cr> 
-  vnoremap p "_dP
-  tnoremap jj <C-\><C-N>
-  tnoremap jk <C-\><C-N>
-  tnoremap kj <C-\><C-N>
-]]
-
 -- LSP
 lvim.lsp.diagnostics.virtual_text = false
 lvim.lsp.override = {"java"}
@@ -35,20 +25,6 @@ lvim.builtin.dashboard.active = true
 lvim.builtin.terminal.active = true
 -- lvim.builtin.nvimtree.side = "left"
 -- lvim.builtin.nvimtree.show_icons.git = 0
-
--- Whichkey
-lvim.builtin.which_key.mappings.l.d = { "<cmd>TroubleToggle<cr>", "Diagnostics" }
-lvim.builtin.which_key.mappings.l.R = { "<cmd>TroubleToggle lsp_references<cr>", "References" }
-lvim.builtin.which_key.mappings.l.o = { "<cmd>SymbolsOutline<cr>", "Outline" }
--- lvim.builtin.which_key.mappings["z"] = { "<cmd>ZenMode<cr>", "Zen" }
-lvim.builtin.which_key.mappings["r"] = {
-  name = "Replace",
-  r = { "<cmd>lua require('spectre').open()<cr>", "Replace" },
-  w = { "<cmd>lua require('spectre').open_visual({select_word=true})<cr>", "Replace Word" },
-  f = { "<cmd>lua require('spectre').open_file_search()<cr>", "Replace Buffer" },
-}
-lvim.builtin.which_key.mappings.G = {"<cmd>Dox<cr>", "Doxygen"}
--- lvim.builtin.which_key.mappings.f = { "<cmd>lua require('lir.float').toggle()<cr>", "Files" }
 
 require("user.builtin").config()
 
--- lua/user/keybindings.lua
+++ lua/user/keybindings.lua
@@ -1,6 +1,29 @@
 local M = {}
 
 M.config = function ()
+  lvim.keys.normal_mode["<S-x>"] = ":BufferClose<CR>"
+  lvim.keys.normal_mode["<esc><esc>"] = "<cmd>nohlsearch<cr>"
+  lvim.keys.normal_mode["Y"] = "y$"
+  lvim.keys.normal_mode["gv"] = "<cmd>vsplit | lua vim.lsp.buf.definition()<cr>"
+  lvim.keys.normal_mode["<C-]>"] = "<cmd>lua vim.lsp.buf.definition()<cr>"
+  lvim.keys.visual_mode["p"] = [["_dP]]
+  -- lvim.keys.insert_mode["jk"] = "<ESC>:w<CR>"
+  lvim.keys.term_mode["jj"] = "<C-\\><C-N>"
+  lvim.keys.term_mode["jk"] = "<C-\\><C-N>"
+  lvim.keys.term_mode["kj"] = "<C-\\><C-N>"
+  -- Whichkey
+  lvim.builtin.which_key.mappings.l.d = { "<cmd>TroubleToggle<cr>", "Diagnostics" }
+  lvim.builtin.which_key.mappings.l.R = { "<cmd>TroubleToggle lsp_references<cr>", "References" }
+  lvim.builtin.which_key.mappings.l.o = { "<cmd>SymbolsOutline<cr>", "Outline" }
+  -- lvim.builtin.which_key.mappings["z"] = { "<cmd>ZenMode<cr>", "Zen" }
+  lvim.builtin.which_key.mappings["r"] = {
+  name = "Replace",
+  r = { "<cmd>lua require('spectre').open()<cr>", "Replace" },
+  w = { "<cmd>lua require('spectre').open_visual({select_word=true})<cr>", "Replace Word" },
+  f = { "<cmd>lua require('spectre').open_file_search()<cr>", "Replace Buffer" },
+}
+  lvim.builtin.which_key.mappings.G = {"<cmd>Dox<cr>", "Doxygen"}
+  -- lvim.builtin.which_key.mappings.f = { "<cmd>lua require('lir.float').toggle()<cr>", "Files" }
   lvim.keys.normal_mode["gv"] = "<cmd>vsplit | lua vim.lsp.buf.definition()<cr>"
 end
 

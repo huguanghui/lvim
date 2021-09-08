51840a4b1dc90eeffe4063166dcd34dd68f7247d
huguanghui <522146829@qq.com>
2021-09-09 07:40:19 +0800

    feat 支持 TabNine 补全插件

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -1,67 +1,81 @@
 local M = {}
 
-M.config = function ()
-  lvim.plugins = {
-    {"editorconfig/editorconfig-vim"},
-    {"folke/tokyonight.nvim"},
-    {"psliwka/vim-smoothie"},
-    {
-       -- 快速移动
-      "phaazon/hop.nvim",
-      event = "BufRead",
-      config = function()
-        require("user.hop").config()
-      end,
-    },
-    {
-      -- 搜索替换
-      "windwp/nvim-spectre",
-      event = "BufRead",
-      config = function()
-        require("user.spectre").config()
-      end,
-    },
-    {
-      "simrat39/symbols-outline.nvim",
-      -- cmd = "SymbolsOutline",
-      config = function()
-        require("user.outline").config()
-      end,
-    },
-    {
-      "folke/todo-comments.nvim",
-      requires = "nvim-lua/plenary.nvim",
-      config = function()
-        require("todo-comments").setup()
-      end,
-      event = "BufRead",
-    },
-    {
-      "lukas-reineke/indent-blankline.nvim",
-      setup = function()
-        require("user.indent_blankline").setup()
-      end,
-      event = "BufRead",
-    },
-    {
-      "folke/trouble.nvim",
-      requires = "kyazdani42/nvim-web-devicons",
-      config = function()
-        require("trouble").setup()
-      end,
-      cmd = "Trouble",
-    },
-    {
-      "danymat/neogen",
-      config = function()
-        require("neogen").setup {
-          enabled = true,
-        }
-      end,
-      event = "BufRead",
-      requires = "nvim-treesitter/nvim-treesitter",
-    },
-  }
+M.config = function()
+	lvim.plugins = {
+		{ "editorconfig/editorconfig-vim" },
+		{ "folke/tokyonight.nvim" },
+		{ "psliwka/vim-smoothie" },
+		{
+			-- 快速移动
+			"phaazon/hop.nvim",
+			event = "BufRead",
+			config = function()
+				require("user.hop").config()
+			end,
+		},
+		{
+			-- 搜索替换
+			"windwp/nvim-spectre",
+			event = "BufRead",
+			config = function()
+				require("user.spectre").config()
+			end,
+		},
+		{
+			"simrat39/symbols-outline.nvim",
+			-- cmd = "SymbolsOutline",
+			config = function()
+				require("user.outline").config()
+			end,
+		},
+		{
+			"folke/todo-comments.nvim",
+			requires = "nvim-lua/plenary.nvim",
+			config = function()
+				require("todo-comments").setup()
+			end,
+			event = "BufRead",
+		},
+		{
+			"tzachar/cmp-tabnine",
+			run = "./install.sh",
+			requires = "hrsh7th/nvim-cmp",
+			config = function()
+				local tabnine = require("cmp_tabnine.config")
+				tabnine:setup({
+					max_lines = 1000,
+					max_num_results = 10,
+					sort = true,
+				})
+			end,
+			disable = not lvim.builtin.tabnine.active,
+		},
+		{
+			"lukas-reineke/indent-blankline.nvim",
+			setup = function()
+				require("user.indent_blankline").setup()
+			end,
+			event = "BufRead",
+		},
+		{
+			"folke/trouble.nvim",
+			requires = "kyazdani42/nvim-web-devicons",
+			config = function()
+				require("trouble").setup()
+			end,
+			cmd = "Trouble",
+		},
+		{
+			"danymat/neogen",
+			config = function()
+				require("neogen").setup({
+					enabled = true,
+				})
+			end,
+			event = "BufRead",
+			requires = "nvim-treesitter/nvim-treesitter",
+		},
+	}
 end
 
 return M

2645b946eef7ae3ef879df8ea1cae5498ebccfab
huguanghui <522146829@qq.com>
2021-10-30 11:06:49 +0800

    fix(copilot): 使用<C-e>替换Tab按键

--- lua/user/builtin.lua
+++ lua/user/builtin.lua
@@ -42,22 +42,17 @@ M.config = function()
   }
 
   if lvim.builtin.sell_your_soul_to_devil then
-    lvim.builtin.cmp.experimental.ghost_text = true
     vim.g.copilot_no_tab_map = true
     vim.g.copilot_assume_mapped = true
     vim.g.copilot_tab_fallback = ""
-    local cmp = require("cmp")
-    lvim.builtin.cmp.mapping["<Tab>"] = function (fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
+    local cmp = require "cmp"
+    lvim.builtin.cmp.mapping["<C-e>"] = function(fallback)
+      cmp.mapping.abort()
+      local copilot_keys = vim.fn["copilot#Accept"]()
+      if copilot_keys ~= "" then
+        vim.api.nvim_feedkeys(copilot_keys, "i", true)
       else
-        local copilot_keys = vim.fn["copilot#Accept"]()
-        if copilot_keys ~= "" then
-          vim.api.nvim_feedkeys(copilot_keys, "i", true)
-        else
-          fallback()
-        end
-
+        fallback()
       end
     end
   end

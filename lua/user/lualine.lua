23a496e0a6b793bd75ecf127633d499d76e79541
huguanghui <522146829@qq.com>
2021-11-25 08:53:07 +0800

    feat: 调整lsp加载进度效果

--- lua/user/lualine.lua
+++ lua/user/lualine.lua
@@ -19,10 +19,42 @@ local function lsp_progress()
   for _, msg in pairs(messages) do
     table.insert(status, (msg.percentage or 0) .. "%% " .. (msg.title or ""))
   end
-  local spinners = { "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" }
+  -- local spinners = { "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" }
+  -- local spinners = { " ", " ", " ", " ", " ", " ", " ", " ", " ", " " }
+  -- local spinners = { " ", " ", " ", " ", " ", " ", " ", " ", " " }
+  local spinners = {
+    " ",
+    " ",
+    " ",
+    " ",
+    " ",
+    " ",
+    " ",
+    " ",
+    " ",
+    " ",
+    " ",
+    " ",
+    " ",
+    " ",
+    " ",
+    " ",
+    " ",
+    " ",
+    " ",
+    " ",
+    " ",
+    " ",
+    " ",
+    " ",
+    " ",
+    " ",
+    " ",
+    " ",
+  }
   local ms = vim.loop.hrtime() / 1000000
-  local frame = math.floor(ms / 120) % #spinners
-  return table.concat(status, " | ") .. " " .. spinners[frame + 1]
+  local frame = math.floor(ms / 60) % #spinners
+  return spinners[frame + 1] .. " " .. table.concat(status, " | ")
 end
 
 vim.cmd [[autocmd User LspProgressUpdate let &ro = &ro]]

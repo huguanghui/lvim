d6f27a47e46fee496190d653a1637a2e9849f10a
huguanghui <522146829@qq.com>
2022-02-14 23:30:26 +0800

    feat(theme): rose-pine匹配最新配置

--- lua/user/theme.lua
+++ lua/user/theme.lua
@@ -37,12 +37,38 @@ M.tokyonight = function()
 end
 
 M.rose_pine = function()
-  vim.g.rose_pine_variant = "moon"
-  vim.g.rose_pine_disable_italics = false
-  vim.g.rose_pine_bold_vertical_split_line = false
-  vim.g.rose_pine_disable_background = lvim.transparent_window
-  vim.g.rose_pine_disable_float_background = true
-  vim.g.rose_pine_inactive_background = false
+  require("rose-pine").setup {
+    ---@usage 'main'|'moon'
+    dark_variant = "moon",
+    bold_vert_split = false,
+    dim_nc_background = false,
+    disable_background = lvim.transparent_window,
+    disable_float_background = true,
+    disable_italics = false,
+    ---@usage string hex value or named color from rosepinetheme.com/palette
+    groups = {
+      border = "highlight_med",
+      comment = "muted",
+      link = "iris",
+      punctuation = "subtle",
+
+      error = "love",
+      hint = "iris",
+      info = "foam",
+      warn = "gold",
+
+      headings = {
+        h1 = "iris",
+        h2 = "foam",
+        h3 = "rose",
+        h4 = "gold",
+        h5 = "pine",
+        h6 = "foam",
+      },
+      -- or set all headings at once
+      -- headings = 'subtle'
+    },
+  }
 end
 
 M.doom = function()

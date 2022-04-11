5dfbc68da0dfb59a2adf7063a6df4a6f71e7f631
huguanghui <522146829@qq.com>
2022-04-11 09:11:47 +0800

    feat 指定treesitter识别的语言

--- lua/user/builtin.lua
+++ lua/user/builtin.lua
@@ -220,7 +220,16 @@ M.config = function()
   -- Treesitter
   -- =========================================
   lvim.builtin.treesitter.context_commentstring.enable = true
-  lvim.builtin.treesitter.ensure_installed = "maintained"
+  local languages = vim.tbl_flatten {
+    { "bash", "c", "c_sharp", "cmake", "comment", "cpp", "css", "dart" },
+    { "dockerfile", "elixir", "elm", "erlang", "fennel", "fish", "go" },
+    { "gomod", "graphql", "hcl", "help", "html", "java", "javascript", "jsdoc" },
+    { "json", "jsonc", "julia", "kotlin", "latex", "ledger", "lua", "make" },
+    { "markdown", "nix", "ocaml", "perl", "php", "python", "query", "r" },
+    { "regex", "rego", "ruby", "rust", "scala", "scss", "solidity" },
+    { "toml", "tsx", "typescript", "vim", "vue", "yaml", "zig" },
+  }
+  lvim.builtin.treesitter.ensure_installed = languages
   lvim.builtin.treesitter.highlight.disable = { "org" }
   lvim.builtin.treesitter.ignore_install = { "haskell", "tlaplus", "php", "org", "vala", "norg" }
   lvim.builtin.treesitter.incremental_selection = {

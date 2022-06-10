7d1ce27e80a89f9a034dd487e486114ff4839d90
huguanghui <522146829@qq.com>
2022-06-10 21:40:22 +0800

    feat(theme) 同步 kanagawa 新配置

--- lua/user/theme.lua
+++ lua/user/theme.lua
@@ -92,12 +92,12 @@ M.kanagawa = function()
   local kanagawa = require "kanagawa"
   kanagawa.setup {
     undercurl = true, -- enable undercurls
-    commentStyle = "NONE",
-    functionStyle = "NONE",
-    keywordStyle = "italic",
-    statementStyle = "italic",
-    typeStyle = "NONE",
-    variablebuiltinStyle = "italic",
+    commentStyle = { italic = true },
+    functionStyle = {},
+    keywordStyle = { italic = true },
+    statementStyle = { bold = true },
+    typeStyle = {},
+    variablebuiltinStyle = { italic = true },
     specialReturn = true, -- special highlight for the return keyword
     specialException = true, -- special highlight for exception handling keywords
     dimInactive = lvim.builtin.global_statusline, -- dim inactive window `:h hl-NormalNC`

3e027e2efe69a2dd98b2003790bb3069de6d823f
huguanghui <522146829@qq.com>
2022-10-22 07:15:12 +0800

    feat 调整定制结构

--- config.lua
+++ config.lua
@@ -62,34 +62,7 @@ lvim.builtin.rust_programming = { active = false } -- rust_tools.nvim + crates.n
 local user = os.getenv "USER"
 if user and (user == "hgh" or user == "yh") then
   lvim.reload_config_on_save = true
-  lvim.builtin.tmux_lualine = false
-  if lvim.builtin.tmux_lualine then
-    vim.opt.cmdheight = 1 -- WARN: =0 is broken on neovim head (https://github.com/neovim/neovim/issues/20281)
-    vim.g.tpipeline_cursormoved = 1
-  end
-  if user == "hgh" then
-    lvim.builtin.custom_web_devicons = true
-    lvim.use_icons = false -- only set to false if you know what are you doing
-  else
-    lvim.builtin.custom_web_devicons = false
-    lvim.use_icons = true -- only set to false if you know what are you doing
-  end
-  lvim.builtin.dap.active = true
-  vim.g.instant_username = user
-  lvim.builtin.collaborative_editing.active = true
-  lvim.builtin.global_statusline = true
-  lvim.builtin.dressing.active = true
-  lvim.builtin.fancy_wild_menu.active = true
-  lvim.builtin.refactoring.active = true
-  lvim.builtin.test_runner.runner = "neotest"
-  lvim.format_on_save = {
-    pattern = "*.rs",
-    timeout = 2000,
-    filter = require("lvim.lsp.utils").format_filter,
-  }
-  lvim.builtin.smooth_scroll = "cinnamon"
-  -- require("lvim.lsp.manager").setup("prosemd_lsp", {})
-  lvim.builtin.rust_programming = { active = true }
+  require("user.custom_user").config()
 end
 if lvim.builtin.winbar_provider == "navic" then
   lvim.builtin.breadcrumbs.active = false
---
+++ lua/user/custom_user.lua
@@ -0,0 +1,36 @@
+local M = {}
+
+M.config = function()
+  -- Your custom config
+  local user = os.getenv "USER"
+  lvim.builtin.tmux_lualine = false
+  if lvim.builtin.tmux_lualine then
+    vim.opt.cmdheight = 1 -- WARN: =0 is broken on neovim head (https://github.com/neovim/neovim/issues/20281)
+    vim.g.tpipeline_cursormoved = 1
+  end
+  if user == "hgh" then
+    lvim.builtin.custom_web_devicons = true
+    lvim.use_icons = false -- only set to false if you know what are you doing
+  else
+    lvim.builtin.custom_web_devicons = false
+    lvim.use_icons = true -- only set to false if you know what are you doing
+  end
+  lvim.builtin.dap.active = true
+  vim.g.instant_username = user
+  lvim.builtin.collaborative_editing.active = true
+  lvim.builtin.global_statusline = true
+  lvim.builtin.dressing.active = true
+  lvim.builtin.fancy_wild_menu.active = true
+  lvim.builtin.refactoring.active = true
+  lvim.builtin.test_runner.runner = "neotest"
+  lvim.format_on_save = {
+    pattern = "*.rs",
+    timeout = 2000,
+    filter = require("lvim.lsp.utils").format_filter,
+  }
+  lvim.builtin.smooth_scroll = "cinnamon"
+  -- require("lvim.lsp.manager").setup("prosemd_lsp", {})
+  lvim.builtin.rust_programming = { active = true }
+end
+
+return M

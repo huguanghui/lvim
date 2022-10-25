b8f5cfa3fec4643ff25aba812762cdf3df2b85d6
huguanghui <522146829@qq.com>
2022-10-25 09:17:54 +0800

    fix(dap) 优化codelldb配置

--- lua/user/dap.lua
+++ lua/user/dap.lua
@@ -1,6 +1,7 @@
 local M = {}
 
 M.config = function()
+  local mason_path = vim.fn.glob(vim.fn.stdpath "data" .. "/mason/")
   local function sep_os_replacer(str)
     local result = str
     local path_sep = package.config:sub(1, 1)
@@ -194,55 +195,22 @@ M.config = function()
   dap.configurations.javascript = dap.configurations.typescript
   dap.configurations.javascriptreact = dap.configurations.typescript
 
+  local path = vim.fn.glob(mason_path .. "packages/codelldb/extension/")
+    or vim.fn.expand "~/" .. ".vscode/extensions/vadimcn.vscode-lldb-1.8.1/"
+  local lldb_cmd = path .. "adapter/codelldb"
 
-  dap.adapters.codelldb = function(on_adapter)
-    local stdout = vim.loop.new_pipe(false)
-    local stderr = vim.loop.new_pipe(false)
+  dap.adapters.codelldb = {
+    type = "server",
+    port = "${port}",
+    executable = {
+      -- CHANGE THIS to your path!
+      command = lldb_cmd,
+      args = { "--port", "${port}" },
 
-    local cmd = vim.fn.expand "~/" .. ".local/share/nvim/dapinstall/codelldb/extension/adapter/codelldb"
-
-    local handle, pid_or_err
-    local opts = {
-      stdio = { nil, stdout, stderr },
-      detached = true,
-    }
-    handle, pid_or_err = vim.loop.spawn(cmd, opts, function(code)
-      stdout:close()
-      stderr:close()
-      handle:close()
-      if code ~= 0 then
-        print("codelldb exited with code", code)
-      end
-    end)
-    assert(handle, "Error running codelldb: " .. tostring(pid_or_err))
-    stdout:read_start(function(err, chunk)
-      assert(not err, err)
-      if chunk then
-        local port = chunk:match "Listening on port (%d+)"
-        if port then
-          vim.schedule(function()
-            on_adapter {
-              type = "server",
-              host = "127.0.0.1",
-              port = port,
-            }
-          end)
-        else
-          vim.schedule(function()
-            require("dap.repl").append(chunk)
-          end)
-        end
-      end
-    end)
-    stderr:read_start(function(err, chunk)
-      assert(not err, err)
-      if chunk then
-        vim.schedule(function()
-          require("dap.repl").append(chunk)
-        end)
-      end
-    end)
-  end
+      -- On windows you may have to uncomment this:
+      -- detached = false,
+    },
+  }
 
   dap.configurations.cpp = {
     {
@@ -257,6 +225,7 @@ M.config = function()
     },
   }
   dap.configurations.c = dap.configurations.cpp
+  dap.configurations.rust = dap.configurations.cpp
 
   -- if lvim.builtin.metals.active then
   --   dap.configurations.scala = {

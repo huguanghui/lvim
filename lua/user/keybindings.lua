fce04fc38c29e63c6f0d655a7f9f2a1a5c7fa255
huguanghui <522146829@qq.com>
2023-02-01 19:13:09 +0800

    update

--- lua/user/keybindings.lua
+++ lua/user/keybindings.lua
@@ -149,10 +149,10 @@ end
 M.config = function()
   -- Additional keybindings
   -- =========================================
-  -- lvim.keys.normal_mode["<CR>"] = {
-  --   "<cmd>lua require('user.neovim').maximize_current_split()<CR>",
-  --   { noremap = true, silent = true, nowait = true },
-  -- }
+  lvim.keys.normal_mode["<CR>"] = {
+    "<cmd>lua if vim.bo.filetype ~= 'alpha' then require('user.neovim').maximize_current_split() else vim.cmd[[unmap <CR>]] end<CR>",
+    { noremap = true, silent = true, nowait = true },
+  }
   lvim.keys.normal_mode["A-a"] = "<C-a>"
   lvim.keys.normal_mode["A-x"] = "<C-x>"
   -- lvim.keys.insert_mode["A-a"] = "<ESC>ggVG<CR>"

ca7354de88e2e3c61df9ea9193ec1ad0f50a316c
huguanghui <522146829@qq.com>
2023-03-29 20:14:28 +0800

    fix 调整主题

--- lua/user/theme.lua
+++ lua/user/theme.lua
@@ -484,6 +484,12 @@ M.telescope_theme = function(colorset)
     link("@lsp.type.interface", "@interface")
     link("@lsp.typemod.function.defaultLibrary", "Special")
     link("@lsp.typemod.variable.defaultLibrary", "@variable.builtin")
+    -- link("@lsp.typemod.variable.global", "@constant.builtin")
+    link("@lsp.typemod.operator", "@operator")
+    link("@lsp.typemod.string", "@string")
+    link("@lsp.typemod.variable", "@variable")
+    link("@lsp.typemod.parameter.label", "@field")
+    link("@type.qualifier", "@keyword")
   end
 
   -- NOTE: these are my personal preferences

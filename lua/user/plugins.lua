a43d07fff47fcb623ce00149f8b2c34cc33962f2
huguanghui <522146829@qq.com>
2024-06-26 21:22:58 +0800

    fix(cinnamon) 同步插件更新

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -224,12 +224,8 @@ M.config = function()
       "declancm/cinnamon.nvim",
       config = function()
         require("cinnamon").setup {
-          default_keymaps = true,
-          default_delay = 4,
-          extra_keymaps = true,
-          extended_keymaps = false,
-          centered = true,
-          scroll_limit = 100,
+          keymaps = { extra = true },
+          options = { delay = 4 },
         }
       end,
       event = "BufRead",
